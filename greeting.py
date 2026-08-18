14781540ceea45229d321af8690411d4ceafdfd2
Karina394 <karinapikul0@gmail.com>
2026-08-18 20:32:01 +0300

    PR1

---
+++ greeting.py
@@ -0,0 +1,13 @@
+name = "Karina"
+surname = "Pikul"
+group = "IT-32"
+birth_year = 2009
+current_year = 2026
+age = current_year - birth_year
+full_name = name + " " + surname
+print("Hello, world!")
+print("my name is", full_name)
+print("group:", group)
+print("i am", age, "years old")
+print("letters in my fullname:", len(full_name))
+print("loud version:", full_name.upper())
